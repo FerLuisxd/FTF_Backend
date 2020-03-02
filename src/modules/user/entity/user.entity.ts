d3b14be3eff384958bd8929c6603ecdd598ce844
Alexander Benavides <ferluisxd@gmail.com>
2020-03-02 15:54:15 -0500

    feat: repository connection and implementation

--- src/modules/user/entity/user.entity.ts
+++ src/modules/user/entity/user.entity.ts
@@ -1,24 +1,37 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 
+@Entity()
 export class User {
-  
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({default:true})
   enabled:boolean
 
+  @Column({default:true})
   activated:boolean
 
+
+  @Column()
   name:string
 
+  @Column()
   username:string
 
+  @Column()
   email:string
 
+  @Column()
   usergroup:string
   
+  @Column()
   lastVisit:Date
 
+  @Column()
   registered:Date
 
+  @Column({default:false})
+  deleted:boolean
 
 }
